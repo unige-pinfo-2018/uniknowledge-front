b789803e5e5de3351454dcfdd68deb3d6e76e3d4
amirh <amir74heidari@gmail.com>
2018-05-15 19:42:24 +0200

    questions in askQuestions

--- src/app/main/content/myQuestions/askQuestionDialog.component.ts
+++ src/app/main/content/myQuestions/askQuestionDialog.component.ts
@@ -1,12 +1,17 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { MatDialog, MatDialogRef, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material';
-
+import { QuestionsService } from '../../../core/services/questions.service'
+import { questions } from '../../../core/models/questions.model'
 
 @Component({
   selector: 'uniKnowledge-askQuestion-dialog-my',
   templateUrl: 'askQuestionDialog.component.html',
 })
 export class AskQuestionDialogComponent {
+  question1: questions = new questions();
+  quesion1From: FormGroup;
+  errors: Object= {};
 
   constructor(
     public dialogRef: MatDialogRef<AskQuestionDialogComponent>,
